2d81b5f92616f031707602dad172afe3f7661ea0
MikusMatvejevs <mikusmatvejevs0@gmail.com>
2026-09-08 09:45:48 +0300

    Projekta sākums

---
+++ src/Pakotne/Kalkulators.java
@@ -0,0 +1,10 @@
+package Pakotne;
+
+public class Kalkulators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Atzimes_Kalkulators {
+}
