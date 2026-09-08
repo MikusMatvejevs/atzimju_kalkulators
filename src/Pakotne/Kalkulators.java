5287890a2b2adc35bb984baf59b3c2e62222120a
MikusMatvejevs <mikusmatvejevs0@gmail.com>
2026-09-08 09:58:00 +0300

    Tiek paprasīts skolēnu skaits ar pārbaudi

--- src/Pakotne/Kalkulators.java
+++ src/Pakotne/Kalkulators.java
@@ -1,10 +1,23 @@
 package Pakotne;
 
+import java.util.Scanner;
+
+import java.util.Scanner;
+
 public class Kalkulators {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int skoleni;
+		Scanner scan = new Scanner(System.in);
+		
+		do {
+			System.out.println("Ievadiet skolēnu skaitu: ");
+			skoleni = scan.nextInt();
+			if (skoleni ==0 || skoleni < 0) {
+				System.out.println("Kļūda! Nekorekti ievadīts skolēnu skaits!");
+			}
+		} while (skoleni==0 || skoleni < 0);
+		
 	}
 
 }
