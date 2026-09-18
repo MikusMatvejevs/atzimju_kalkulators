12f95edaa7bd94290737a781d8c5fe2afd99ac34
MikusMatvejevs <mikusmatvejevs0@gmail.com>
2026-09-18 09:13:05 +0300

    Salabota nolasīšanas metode. Pielāgota tā, lai to varētu izsaukt datu kārtošanas laikā. Uzsākta kārtošanas metodes izstrāde.

--- src/Pakotne/kartosana.java
+++ src/Pakotne/kartosana.java
@@ -1,10 +1,19 @@
 package Pakotne;
 
+import java.util.Scanner;
+
 public class kartosana {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	Scanner dati = new Scanner(System.in);
+	System.out.println("1 - Kārtot augošā secība\n "
+					+ "2 - kārtot dilstošā secībā\n "
+					+ "q - Atcelt");
+	int ievade = dati.nextInt();
+	
+	switch()
+	
 	}
 
 }
--- src/Pakotne/nolasitd.java
+++ src/Pakotne/nolasitd.java
@@ -1,10 +1,41 @@
 package Pakotne;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class nolasitd {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    static ArrayList<Double> nolasit_gala() {
 
-	}
+        ArrayList<Double> atzimes = new ArrayList<>();
 
+        try {
+            File fails = new File("rezultati.txt");
+            Scanner lasa = new Scanner(fails);
+
+            while (lasa.hasNextLine()) {
+                String rinda = lasa.nextLine();
+
+                if (rinda.startsWith("Gala vērtējums:")) {
+                    String vertiba = rinda.replace("Gala vērtējums:", "").trim();
+
+                    // 8,5 pārveido par 8.5
+                    vertiba = vertiba.replace(",", ".");
+
+                    double atzime = Double.parseDouble(vertiba);
+
+                    atzimes.add(atzime);
+                }
+            }
+
+            lasa.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Fails netika atrasts!");
+        }
+
+        return atzimes;
+    }
 }
